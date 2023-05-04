ca2a8a9d406b83e06b1ef379161bc57b7faf043b
ahmed123refaat <125468663+ahmed123refaat@users.noreply.github.com>
2023-05-04 03:01:23 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,68 +5,71 @@
  * myprintf - Printf function.
  * Return: Printed chars.
  */
-int myprintf(const char *format, ...)
+int my_printf(const char *format, ...)
 {
-va_list p;
-va_start(p, format);
-int i = 0;
-while (format[i])
-{
-if (format[i] == '%')
-i++;
-switch (format[i])
-{
-case 'd':
-{
-int x = va_arg(p, int);
-printf("%d", x);
-break;
-}
-case 'f':
-{
-double x = va_arg(p, double);
-printf("%f", x);
-break;
-}
-case 'c':
-{
-int x = va_arg(p, char);
-printf("%c", x);
-break;
-}
-case 's':
-{
-char *x = va_arg(p, char*);
-printf("%s", x);
-break;
-}
-case 'x':
-case 'X':
-{
-int x = va_arg(p, int);
-printf("%x", x);
-break;
-}
-case 'p':
-{
-void *x = va_arg(p, void*);
-printf("%p", x);
-break;
-}
-case '%':
-putchar('%');
-break;
-default:
-putchar(format[i]);
-break;
-}
-}
-else
-{
-putchar(format[i]);
-}
-i++;
-}
-va_end(p);
-return 0;
+    va_list args;
+    va_start(args, format);
+
+    int i = 0;
+    while (format[i])
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            switch (format[i])
+            {
+                case 'd':
+                {
+                    int x = va_arg(args, int);
+                    printf("%d", x);
+                    break;
+                }
+                case 'f':
+                {
+                    double x = va_arg(args, double);
+                    printf("%f", x);
+                    break;
+                }
+                case 'c':
+                {
+                    int x = va_arg(args, int);
+                    printf("%c", x);
+                    break;
+                }
+                case 's':
+                {
+                    char *x = va_arg(args, char*);
+                    printf("%s", x);
+                    break;
+                }
+                case 'x':
+                case 'X':
+                {
+                    int x = va_arg(args, int);
+                    printf("%x", x);
+                    break;
+                }
+                case 'p':
+                {
+                    void *x = va_arg(args, void*);
+                    printf("%p", x);
+                    break;
+                }
+                case '%':
+                    putchar('%');
+                    break;
+                default:
+                    putchar(format[i]);
+                    break;
+            }
+        }
+        else
+        {
+            putchar(format[i]);
+        }
+        i++;
+    }
+
+    va_end(args);
+    return 0;
 }
