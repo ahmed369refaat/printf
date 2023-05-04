5b5bbc76d5766e8d6a52c2350ff9bf5bea0d7678
ahmed123refaat <125468663+ahmed123refaat@users.noreply.github.com>
2023-05-04 03:26:51 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -7,8 +7,8 @@
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
+    va_list list;
+    va_start(list, format);
     int i;
     while (format[i])
     {
@@ -19,38 +19,38 @@ int _printf(const char *format, ...)
             {
                 case 'd':
                 {
-                    int x = va_arg(args, int);
+                    int x = va_arg(list, int);
                     printf("%d", x);
                     break;
                 }
                 case 'f':
                 {
-                    double x = va_arg(args, double);
+                    double x = va_arg(list, double);
                     printf("%f", x);
                     break;
                 }
                 case 'c':
                 {
-                    int x = va_arg(args, int);
+                    int x = va_arg(list, int);
                     printf("%c", x);
                     break;
                 }
                 case 's':
                 {
-                    char *x = va_arg(args, char*);
+                    char *x = va_arg(list, char*);
                     printf("%s", x);
                     break;
                 }
                 case 'x':
                 case 'X':
                 {
-                    int x = va_arg(args, int);
+                    int x = va_arg(list, int);
                     printf("%x", x);
                     break;
                 }
                 case 'p':
                 {
-                    void *x = va_arg(args, void*);
+                    void *x = va_arg(list, void*);
                     printf("%p", x);
                     break;
                 }
